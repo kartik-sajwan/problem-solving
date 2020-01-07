4b2e68dcb8117317b7af1d811fd21e9e6f71d518
infinifier <kartiksajwan.dev@gmail.com>
2020-01-07 21:54:44 +0530

    cpp template example

--- current/temp.cpp
+++ current/temp.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main()  {
-    
+    int t, counter, temp;
+    char str[400], Q[400], S[400];
+    cin>>t;
+    while(t--)  {
+        scanf("%s", str);
+        counter = 0;
+        while(str)  {
+            temp = (int)str[i];
+            if(temp >= 97 && temp <=122)    {
+                
+            }
+        }
+    }
     return 0;
 }
