23cd58813ac1c23e99fe0f30983cd04b82dd1eb6
infinifier <kartiksajwan.dev@gmail.com>
2020-01-09 23:17:41 +0530

    temp_savepoint

--- current/temp.cpp
+++ current/temp.cpp
@@ -1,21 +1,38 @@
 #include<stdio.h>
 #include<iostream>
+#include<exception>
 
 using namespace std;
 
+
+class E : public std::exception {
+public:
+    explicit E (const char * s) throw() : msg(s)    {   }
+    const char* what() const throw() { return msg; }
+private:
+    E() {};
+    const char * msg;
+};
+
+
+template <typename T>
+class Stack {
+private:
+    static const int defaultSize = 10;
+    static const int maxSize = 1000;
+    int _size;
+    int _top;
+    T * stackPtr;
+public:
+    explicit Stack (int);
+    ~Stack()    { delete[] stackPdtr; };
+    T & push( const T & );
+    T & pop();
+    bool isEmpty() const { return _top < 0; }
+
+}
+
 int main()  {
-    int t, counter, temp;
-    char str[400], Q[400], S[400];
-    cin>>t;
-    while(t--)  {
-        scanf("%s", str);
-        counter = 0;
-        while(str)  {
-            temp = (int)str[i];
-            if(temp >= 97 && temp <=122)    {
-                
-            }
-        }
-    }
+    
     return 0;
 }
