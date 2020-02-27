5daffab75a5ed74d9cb56df9148aff410356be3d
infinifier <kartiksajwan.dev@gmail.com>
2020-02-27 21:34:37 +0530

    Rearangement

--- current/temp.cpp
+++ current/temp.cpp
@@ -8,14 +8,41 @@ using namespace std;
 
 int main()	{
 
-	long int t;
-	long long int c, n, times, diff ;
-	long long int sum, sum_n, bak_n;
+	int t, n, flag, i;
+	long long int a[20], my_sum, sum;
 
 	cin >> t;
 	while(t--)	{
-		cin >> 
 
+		cin >> n;
+		cin>>sum;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		sort(a, a+n, greater<int>());
+		i=0;
+		flag = 0;
+		my_sum = 0;
+		while(i<=n)	{
+			if(my_sum == sum)	{
+				my_sum += a[i];
+				cout<< "YES\n";
+				flag = 1;
+			}
+			else if(my_sum < sum && a[i] > 0)	{
+				if(my_sum+a[i] <= sum)
+					my_sum += a[i];
+			}
+			else if(my_sum > sum && a[i] < 0)	{
+				if(my_sum+a[i] >= sum)
+					my_sum += a[i];
+			}
+			i++;
+			cout<<my_sum<<"\t";
+		}
+		if(!flag)
+			cout << "NO";
 	}
 	return 0;
 }
